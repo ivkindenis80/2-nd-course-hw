e1578926b15588bd1e04c851325b825a6c838f50
DENIS1508800 <promosait80@gmail.com>
2023-10-11 10:26:10 +0300

    Home work is complet

---
+++ js/hw3/index.js
@@ -0,0 +1,74 @@
+// Задание 1
+// let password = 'Кино';
+// let enteringPassword = prompt('Введите пароль');
+
+// if (enteringPassword === password){
+//     alert('Пароль введен верно');
+// }else{
+//     alert('Пароль введен неправильно')
+// };
+
+// Задаение 2
+// let enteringNumber = Number (prompt('Введите любое число'));
+
+// if (enteringNumber >=0 && enteringNumber <=10){
+//     alert('Верно');
+// }else{
+//     alert('Неверно')
+// };
+
+// Задание 3
+// let d = 5;
+// let e = 10;
+// if (d>=100 || e>=100){
+//     alert('Верно');
+// }else{
+//     alert('Неверно');
+// };
+
+// Задание 4
+// let a = '2';
+// let b = '3';
+
+// alert (Number(a) + Number(b));
+
+// Задание 5 
+// let monthNumber = String (prompt('Введите номер месяца'));
+// switch (monthNumber) {
+//     case '1':
+//     case '2':
+//     case '12':
+//         console.log('Зима');
+//         break;
+//     case '3':
+//     case '4':
+//     case '5':
+//         console.log('Весна');
+//         break;
+//     case '6':
+//     case '7':
+//     case '8':
+//         console.log('Лето');
+//         break;
+//     case '9':
+//     case '10':
+//     case '11':
+//         console.log('Осень');
+//         break;
+
+//     default:
+//         console.log('Нету такого месяца');
+//         break;
+// }
+
+// Задание 7 
+// let number = Number(prompt('Пожалуйста, введите любое число'));
+// if (number % 2){
+//      alert('Число нечетное');
+// }else{
+//      alert('Число четное');
+// }
+
+// Задание 8
+// Задание 9 
+
