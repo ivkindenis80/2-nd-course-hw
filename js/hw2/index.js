22c587bdcabb3384e03edd0a8c4e63e280bdad67
DENIS1508800 <promosait80@gmail.com>
2023-10-11 07:22:12 +0300

    Home work is  complet

---
+++ js/hw2/index.js
@@ -0,0 +1,73 @@
+// Задание 1
+// let a = 10;
+// a = 20;
+// alert(a);
+
+// // // Задание 2
+// let iPhone = 2007;
+// alert(iPhone);
+
+// // // Задание 3
+// let Jname = 'Brendan Eyck';
+// alert(Jname);
+
+// // // Задание 4
+// let b = 10;
+// let c = 2;
+// b = (b/c);
+// alert(b);
+
+
+// // // Задание 5
+// let result = 2**5;
+// alert(result);
+
+
+// // Задание 6
+// let a = 9;
+// let b = 2;
+// a= a % b;
+// alert(a);
+
+// // Задание 7 (нузнаю)
+// let num = 1;
+// num += 5;
+// num =- 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+// // // Задание 8
+// let age = prompt("Сколько вам лет?");
+// alert(age);
+
+// // Задание 9.0
+// let user = {
+//     name:'Boris',
+//     age: 28,
+//     isAdmin:true,
+// }
+// // Задание 9.1
+// user["city of residence"] = 'Тула';
+// // Задание 9.2
+// user.age = 30;
+// // Задание 9.3
+// delete user["city of residence"];
+// console.log(user);
+
+// Задание 9.4   не знаю решения
+// let info = prompt('Какую информацию хотите узнать о пользователе?');
+// let user = {
+//     name:'Boris',
+//     age: 28,
+//     "isAdmin":true,
+//  }
+// alert(user[info]);
+
+// Задание 10
+let info = prompt('Назовите своё имя');
+alert(`Привет ${info}"!"`);
+
+
